a9943e5ee5204d93e252ed0c74698d0f9d800c27
FeatCodna <ohji1006@naver.com>
2020-07-28 00:07:19 +0900

    make reservation confirm, cancel list

--- src/main/resources/js/myReservation/totalInfo.js
+++ src/main/resources/js/myReservation/totalInfo.js
@@ -1,6 +1,8 @@
-class Server{
+class Builder{
     constructor(){
         this.email = document.location.search.split("=")[1];
+        this.confirmRsv = document.querySelector(".card.confirmed");
+        this.cancelRsv = document.querySelector(".card.used.cancel");
         this.rsvInfo = null;
     }
 
@@ -9,11 +11,13 @@ class Server{
     }
 
     static async create(){
-        let srv = new Server();
-        srv.rsvInfo = await srv.getRsvInfo();
-        console.log(srv.rsvInfo);
+        let buildIns = new Builder();
+        buildIns.rsvInfo = await buildIns.getRsvInfo();
+        console.log(JSON.stringify(buildIns.rsvInfo));
 
-        return srv;
+        buildIns.makePage();
+
+        return buildIns;
     }
 
     getRsvInfo(){
@@ -22,9 +26,92 @@ class Server{
             httpRequest.addEventListener("load",()=>{
                 resolve(JSON.parse(httpRequest.responseText));
             });
-            httpRequest.open('GET', Server.RSVINFOURL +'?reservationEmail='+ this.email);
+            httpRequest.open('GET', Builder.RSVINFOURL +'?reservationEmail='+ this.email);
             httpRequest.send();
         })
     }
 
+    makePage(){
+        let confirmTicketInfo = this.getConFirmTicketInfo();
+        let cancleTicketInfo = this.getCancleTicketInfo();
+
+        let confirmHtml = this.makeTicket(confirmTicketInfo);
+        let cancleHtml = this.makeTicket(cancleTicketInfo);
+
+        this.confirmRsv.insertAdjacentHTML('beforeend', confirmHtml);
+        this.cancelRsv.insertAdjacentHTML('beforeend', cancleHtml);
+    }
+
+    getConFirmTicketInfo(){
+        let reservationArr = this.rsvInfo["reservations"];
+        let confirmTicketInfo = { 'reservations' : [] };
+
+        reservationArr.forEach((elm)=>{
+            if(elm['cancelYn'] === false){
+                confirmTicketInfo['reservations'].push(elm);
+            }
+        });
+
+        return confirmTicketInfo;
+    }
+    getCancleTicketInfo(){
+        let reservationArr = this.rsvInfo["reservations"];
+        let cancleicketInfo = { 'reservations' : [] };
+
+        reservationArr.forEach((elm)=>{
+            if(elm['cancelYn'] === true){
+                cancleicketInfo['reservations'].push(elm);
+            }
+        });
+
+        return cancleicketInfo;
+    }
+
+    makeTicket(rsvInfo){
+        let template = document.querySelector("#rsvConfirmed").innerHTML;
+        let ticketInfoFactory = Handlebars.compile(template);
+
+        return ticketInfoFactory(rsvInfo);
+    }
 }
+
+Handlebars.registerHelper('RsvId', (rsvId) => {
+    const rsvNoLength = 7;
+    rsvId = rsvId+'';
+    
+    for(let idx = 0; idx< rsvNoLength - rsvId.length; idx++){
+        rsvId = '0' + rsvId;
+    }
+
+    return 'No.' + rsvId;
+});
+
+Handlebars.registerHelper('Price', (price) => {
+    let localePrice = parseInt(price).toLocaleString();
+    return `${localePrice}`;
+});
+
+Handlebars.registerHelper('CancleBtn', (cancelYn) => {
+    if(cancelYn  === false){
+        return "";
+    }
+
+    return `<div class="booking_cancel"> <button class="btn"><span>취소</span></button> </div>`;
+});
+
+// summary board class 생성.
+
+// 빌드 class 가 필요.
+//     server 를 통해 각각의 category 에 예약정보 build 후 category 에 분배.
+//     예약 category 에 있는 버튼들에 대해 취소요청하는 callback 함수 등록
+//     response 값이 적절하게 return 되면, categorymanager 에 id 를 넘겨주고 이동해달라고 요청
+
+// categorymanager  class 생성. 
+// 취소시 기존카테고리에서 취소 카테고리로 이동 함수 필요.
+
+// 취소 카테고리에서 id 별로 sort 하는 함수 필요.
+
+
+
+
+
