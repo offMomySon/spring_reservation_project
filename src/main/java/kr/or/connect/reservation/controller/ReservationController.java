8e8971424dc436eefb25f6c142df942dcc33bcac
FeatCodna <ohji1006@naver.com>
2020-08-01 04:42:03 +0900

    fix to keep email login at detail page

--- src/main/java/kr/or/connect/reservation/controller/ReservationController.java
+++ src/main/java/kr/or/connect/reservation/controller/ReservationController.java
@@ -1,13 +1,27 @@
 package kr.or.connect.reservation.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mysql.cj.Session;
 
 @Controller
 public class ReservationController {
 
 	@GetMapping(path = "/htmls/mainpage.html")
-	public String showMainPage() {
+	public String showMainPage(HttpSession session) {
 		return "mainpage";
+	}
+	
+	@GetMapping(path= "/htmls/detail.html")
+	public String showDetailPage(HttpSession session) {
+		return "detail";
+	}
+	
+	@GetMapping(path= "/htmls/reserve.html")
+	public String showReservePage(HttpSession session) {
+		return "reserve";
 	}
 }
