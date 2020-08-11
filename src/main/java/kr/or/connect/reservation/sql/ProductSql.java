c1d76ff281fe773f4505daac6ae63ce5dde79424
FeatCodna <ohji1006@naver.com>
2020-08-11 20:51:04 +0900

    fix wrong sql

--- src/main/java/kr/or/connect/reservation/sql/ProductSql.java
+++ src/main/java/kr/or/connect/reservation/sql/ProductSql.java
@@ -16,7 +16,7 @@ public class ProductSql {
 			+ "join product as pr on ca.id = pr.category_id "
 			+ "join display_info as di on pr.id = di.product_id "
 			+ "join product_image as pi on pr.id = pi.product_id "
-			+ "join file_info as fi on pi.file_info_id = fi.id "
+			+ "join file_info as fi on pi.file_id = fi.id "
 			+ "where pi.type = 'th' " + "LIMIT :start, :end";
 
 	public static final String SELECT_SPECIFIC_CATEGORY_PRODUCT = 
--- src/main/java/kr/or/connect/reservation/sql/PromotionSql.java
+++ src/main/java/kr/or/connect/reservation/sql/PromotionSql.java
@@ -4,6 +4,6 @@ public class PromotionSql {
 	public final static String SELECT_ALL = "select pm.id, pd.id as product_id, fi.save_file_name as product_image_url from promotion as pm " + 
 			"join product as pd on pm.product_id = pd.id " + 
 			"join product_image as pi on pd.id = pi.product_id " + 
-			"join file_info as fi on pi.file_info_id = fi.id " + 
+			"join file_info as fi on pi.file_id = fi.id " + 
 			"where pi.type = 'th' ";
 }
