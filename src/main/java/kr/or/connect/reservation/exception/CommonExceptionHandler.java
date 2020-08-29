13a5a92bb04c5ae996882fa1cd4dddd0353bf44c
FeatCodna <ohji1006@naver.com>
2020-08-29 21:06:09 +0900

    handle with reservation

--- src/main/java/kr/or/connect/reservation/exception/CommonExceptionHandler.java
+++ src/main/java/kr/or/connect/reservation/exception/CommonExceptionHandler.java
@@ -7,20 +7,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class CommonExceptionHandler {
 	
 	@ExceptionHandler(NullPointerException.class)
 	public ResponseEntity<ApiErrorResponse>  handleNullPointerException(NullPointerException ex){
 		//error-0000 mean, NullPointerException
 		ApiErrorResponse response = new ApiErrorResponse("error-0000", "SomeThing wrong, please send another request.");
-	
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-	}
-	
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ApiErrorResponse>  handleException(Exception ex){
-		//error-0001 mean, Exception non expected error
-		ApiErrorResponse response = new ApiErrorResponse("error-0001", "SomeThing wrong, please send another request.");
 	
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
 	}
