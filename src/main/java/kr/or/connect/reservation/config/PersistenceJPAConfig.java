9902f4388eeec1b5c9ad116d94145217df252344
FeatCodna <ohji1006@naver.com>
2020-10-21 01:05:36 +0900

    move to intellij

--- src/main/java/kr/or/connect/reservation/config/PersistenceJPAConfig.java
+++ src/main/java/kr/or/connect/reservation/config/PersistenceJPAConfig.java
@@ -1,38 +1,23 @@
 package kr.or.connect.reservation.config;
 
-import java.util.Properties;
-
-import javax.sql.DataSource;
-
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:application.properties")
-@ConfigurationProperties
 @EnableConfigurationProperties
 @SpringBootApplication
 public class PersistenceJPAConfig {
-	@Autowired
-	DataSource dataSource;
 
 	private String driverClassName = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://3.134.162.196:3306/connectdb?serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true&useSSL=false&characterEncoding=utf8";
