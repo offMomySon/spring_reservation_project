88a3ba857216d09c8ced835d5109ab6a590f43c1
offMomySon <ohji100629@naver.com>
2021-02-19 01:04:23 +0900

    change category count query api

--- src/main/java/kr/or/connect/reservation/service/impl/CategoryServiceImpl.java
+++ src/main/java/kr/or/connect/reservation/service/impl/CategoryServiceImpl.java
@@ -2,7 +2,6 @@ package kr.or.connect.reservation.service.impl;
 
 import kr.or.connect.reservation.dto.CategoryResult;
 import kr.or.connect.reservation.model.Category;
-import kr.or.connect.reservation.model.Product;
 import kr.or.connect.reservation.repository.CategoryRepository;
 import kr.or.connect.reservation.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,13 +26,7 @@ public class CategoryServiceImpl implements CategoryService {
         List<CategoryResult> categoryResults = categories
                 .stream()
                 .map(category -> {
-                    //데이터 커지면 통계쿼리 처리가 좋을수도 있음.
-                    List<Product> products = category.getProducts();
-
-                    int count = products
-                            .stream()
-                            .map(product -> product.getDisplayInfos().size())
-                            .reduce(0, Integer::sum);
+                    long count = categoryRepository.countProductDisplayInfo(category.getId());
 
                     return new CategoryResult(category.getId(), category.getName(), count);
                 })
