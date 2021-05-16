6c92d88a1cf53b54bc4f5be8080cdc0b53431aaf
offMomySon <43500275+offMomySon@users.noreply.github.com>
2021-05-16 21:57:20 +0900

    Revert "Feature/refactor multimodule sms service"

--- reflectPromotion-module/src/main/java/com/module/count/infrastructure/config/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.module.count.infrastructure.config.repository;
-
-import com.module.count.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
